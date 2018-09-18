93c8e06b9c941fc2ff70f567c2de24378afd68cd
henriquearaujo91 <henrique_araujo91@outlook.com>
2018-09-18 01:01:32 -0300

    Modulo de seguranca removido

---
+++ medclin/src/main/java/br/com/medclin/model/User.java
@@ -0,0 +1,69 @@
+package br.com.medclin.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty
+	@Column(unique = true)
+	private String username;
+	@NotEmpty
+	@JsonIgnore
+	private String password;
+	@NotEmpty
+	private String name;
+	@NotEmpty
+	private boolean admin;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
+
+}
