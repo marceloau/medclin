81dae5236ec115d0e6388227e8250a1db0be7089
henrique.a.a.santos <henrique.a.a.santos@CPX-XFJFJ3M9T9P.dir.svc.accenture.com>
2018-11-26 23:06:17 -0200

    Ajuste referente aos campos de auditoria do exame, tipoExame, medicamento e tipoMedicamento

--- medclin/src/main/java/br/com/medclin/common/AuditoriaUtil.java
+++ medclin/src/main/java/br/com/medclin/common/AuditoriaUtil.java
@@ -8,18 +8,26 @@ import org.springframework.context.annotation.Configuration;
 import br.com.medclin.model.ContatoPessoa;
 import br.com.medclin.model.Especialidade;
 import br.com.medclin.model.EspecialidadeMedico;
+import br.com.medclin.model.Exame;
+import br.com.medclin.model.Medicamento;
 import br.com.medclin.model.Medico;
 import br.com.medclin.model.Operadora;
 import br.com.medclin.model.Paciente;
 import br.com.medclin.model.TipoContato;
+import br.com.medclin.model.TipoExame;
+import br.com.medclin.model.TipoMedicamento;
 import br.com.medclin.model.TipoPlanoSaude;
 import br.com.medclin.repository.ContatoPessoaRepository;
 import br.com.medclin.repository.EspecialidadeMedicoRepository;
 import br.com.medclin.repository.EspecialidadeRepository;
+import br.com.medclin.repository.ExameRepository;
+import br.com.medclin.repository.MedicamentoRepository;
 import br.com.medclin.repository.MedicoRepository;
 import br.com.medclin.repository.OperadoraRepository;
 import br.com.medclin.repository.PacienteRepository;
 import br.com.medclin.repository.TipoContatoRepository;
+import br.com.medclin.repository.TipoExameRepository;
+import br.com.medclin.repository.TipoMedicamentoRepository;
 import br.com.medclin.repository.TipoPlanoSaudeRepository;
 
 @Configuration
@@ -48,6 +56,18 @@ public class AuditoriaUtil {
 
 	@Autowired
 	private ContatoPessoaRepository contatoPessoaRep;
+
+	@Autowired
+	private ExameRepository exameRep;
+
+	@Autowired
+	private TipoExameRepository tipoExameRep;
+
+	@Autowired
+	private MedicamentoRepository medicamentoRep;
+
+	@Autowired
+	private TipoMedicamentoRepository tipoMedicamentoRep;
 
 	private final String FLAG_SIM = "S";
 
@@ -92,6 +112,26 @@ public class AuditoriaUtil {
 			((EspecialidadeMedico) entidade).setFlagAtivo(FLAG_SIM);
 			((EspecialidadeMedico) entidade).setUsuarioUltimaAlteracao(matricula);
 			((EspecialidadeMedico) entidade).setDataCriacao(new Date());
+		} else if (entidade instanceof Exame) {
+			((Exame) entidade).setDataUltimaAlteracao(new Date());
+			((Exame) entidade).setFlagAtivo(FLAG_SIM);
+			((Exame) entidade).setUsuarioUltimaAlteracao(matricula);
+			((Exame) entidade).setDataCriacao(new Date());
+		} else if (entidade instanceof TipoExame) {
+			((TipoExame) entidade).setDataUltimaAlteracao(new Date());
+			((TipoExame) entidade).setFlagAtivo(FLAG_SIM);
+			((TipoExame) entidade).setUsuarioUltimaAlteracao(matricula);
+			((TipoExame) entidade).setDataCriacao(new Date());
+		} else if (entidade instanceof Medicamento) {
+			((Medicamento) entidade).setDataUltimaAlteracao(new Date());
+			((Medicamento) entidade).setFlagAtivo(FLAG_SIM);
+			((Medicamento) entidade).setUsuarioUltimaAlteracao(matricula);
+			((Medicamento) entidade).setDataCriacao(new Date());
+		} else if (entidade instanceof TipoMedicamento) {
+			((TipoMedicamento) entidade).setDataUltimaAlteracao(new Date());
+			((TipoMedicamento) entidade).setFlagAtivo(FLAG_SIM);
+			((TipoMedicamento) entidade).setUsuarioUltimaAlteracao(matricula);
+			((TipoMedicamento) entidade).setDataCriacao(new Date());
 		}
 	}
 
@@ -136,6 +176,26 @@ public class AuditoriaUtil {
 			((EspecialidadeMedico) entidade).setDataCriacao(especialidadeMedicoRep
 					.buscarDataCriacaoPorCodigo(((EspecialidadeMedico) entidade).getEspecialidadeMedicoPK()));
 			((EspecialidadeMedico) entidade).setUsuarioUltimaAlteracao(matricula);
+		} else if (entidade instanceof Exame) {
+			((Exame) entidade).setDataUltimaAlteracao(new Date());
+			((Exame) entidade).setDataCriacao(exameRep
+					.buscarDataCriacaoPorCodigo(((Exame) entidade).getCodigoExame()));
+			((Exame) entidade).setUsuarioUltimaAlteracao(matricula);
+		}else if (entidade instanceof TipoExame) {
+			((TipoExame) entidade).setDataUltimaAlteracao(new Date());
+			((TipoExame) entidade).setDataCriacao(tipoExameRep
+					.buscarDataCriacaoPorCodigo(((TipoExame) entidade).getCodigoTipoExame()));
+			((TipoExame) entidade).setUsuarioUltimaAlteracao(matricula);
+		}else if (entidade instanceof Medicamento) {
+			((Medicamento) entidade).setDataUltimaAlteracao(new Date());
+			((Medicamento) entidade).setDataCriacao(medicamentoRep
+					.buscarDataCriacaoPorCodigo(((Medicamento) entidade).getCodigoMedicamento()));
+			((Medicamento) entidade).setUsuarioUltimaAlteracao(matricula);
+		} else if (entidade instanceof TipoMedicamento) {
+			((TipoMedicamento) entidade).setDataUltimaAlteracao(new Date());
+			((TipoMedicamento) entidade).setDataCriacao(tipoMedicamentoRep
+					.buscarDataCriacaoPorCodigo(((TipoMedicamento) entidade).getCodigoTipoMedicamento()));
+			((TipoMedicamento) entidade).setUsuarioUltimaAlteracao(matricula);
 		}
 	}
 
