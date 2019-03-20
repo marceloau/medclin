bbb14501c681a806550d27265e15e92bcf4782af
Marcelo Aurino <marceloaurino2207@gmail.com>
2019-03-19 23:22:59 -0300

    Ajuste de segurança para permitir requisições de PUT e Options.

--- medclin/src/main/java/br/com/medclin/config/SecurityConfig.java
+++ medclin/src/main/java/br/com/medclin/config/SecurityConfig.java
@@ -43,6 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/medclin/especialidade/**"
 	};
 	
+	private static final String[] PUBLIC_MATCHERS_OPTIONS = {
+			"/medclin/especialidade/**"
+	};
+	
 	@Autowired
 	private UserDetailsService userDetailsService;
 	
@@ -56,6 +60,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
 		.antMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
+		.antMatchers(HttpMethod.OPTIONS, PUBLIC_MATCHERS_OPTIONS).permitAll()
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
@@ -73,7 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
 		configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
 	
